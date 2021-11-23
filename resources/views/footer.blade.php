936726289ba6957520b6472647cfe705a696cee1
1234Sumuu <sumaiya.akter.cse@ulab.edu.bd>
2021-11-23 10:51:03 -0800

    new laravel online shop

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,11 @@
+<div style="clear:both" class="panel panel-default">
+    <div class="panel-body">
+      Contact Us
+    </div>
+    <div class="panel-footer">
+        Email: shop@gmail.com <br>
+        phone: +096 6526372227 <br>
+        Address: 22/67 New Col, UAE. <br>
+
+    </div>
+</div>
