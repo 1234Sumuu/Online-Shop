eb15a5729344cfbd237cd74b31abcbeef5f5ba96
1234Sumuu <sumaiya.akter.cse@ulab.edu.bd>
2021-11-23 18:29:46 -0800

    product detail add to car

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use view;
 use index;
 use App\Models\Product;
+use App\Models\Cart;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -18,5 +19,28 @@ class ProductController extends Controller
     {
         $data =Product::find($id);
        return view('detail',['product'=>$data]);
+    }
+    function search(Request $req)
+    {
+        $data= Product::
+        where('name', 'like', '%'.$req->input('query').'%')
+        ->get();
+        return view('search',['products'=>$data]);
+    }
+    function addToCart(Request $req)
+    {
+        if($req->session()->has('user'))
+        {
+           $cart= new Cart;
+           $cart->user_id=$req->session()->get('user')['id'];
+           $cart->product_id=$req->product_id;
+           $cart->save();
+           return redirect('/');
+
+        }
+        else
+        {
+            return redirect('/login');
+        }
     }
 }
